9eebe215ef656d4ab16f455649572461c91ae8b3
Abhii1564 <abhirasaniya15@gmail.com>
2024-03-30 11:36:28 +0530

    Untiteld

---
+++ travalingapp/lib/Screens/booking_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookingPageScreen extends StatefulWidget {
+  const BookingPageScreen({super.key});
+
+  @override
+  State<BookingPageScreen> createState() => _BookingPageScreenState();
+}
+
+class _BookingPageScreenState extends State<BookingPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
